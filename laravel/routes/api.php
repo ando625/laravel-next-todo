c9150243885327e3cdedd61f0f0a5f63205d2005
ando.yoshie <ando.yoshie1111@gmail.com>
2026-05-27 10:36:46 +0900

    CRUD処理のapiルート作成

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+
+Route::middleware('auth:sanctum')->group(function (){
+
+
+    //todo一覧表示
+    Route::get('/todos', [TodoController::class, 'todoIndex']);
+
+    //todo作成
+    Route::post('/todos', [TodoController::class,'todoStore']);
+
+    //todo編集
+    Route::put('/todos/{id}',[TodoController::class,'todoUpdate']);
+
+    //todo削除
+    Route::delete('/todos/{id}', [TodoController::class,'todoDelete']);
 });
